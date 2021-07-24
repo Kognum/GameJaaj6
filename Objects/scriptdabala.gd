ed0d7e04268493e8683cf4b6c64cf470f4df02cc
Grand Ed <eduloko12@gmail.com>
2021-07-24 15:16:01 -0300

    vai dar conflito KKKKKKKKKKKKK

--- Objects/scriptdabala.gd
+++ Objects/scriptdabala.gd
@@ -5,8 +5,6 @@ export var bulletspeed = 1150.0
 export var basetilt = 0.05
 
 var slighttilt : float
-
-var rng = RandomNumberGenerator.new()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,21 +12,20 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rng.randomize()
-	slighttilt = rng.randf_range(-basetilt, basetilt)
+	slighttilt = rand_range(-basetilt, basetilt)
 	self.global_rotation = global_rotation + slighttilt
 	print(slighttilt)
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += (Vector2.RIGHT*bulletspeed).rotated(rotation) * delta
-
-
 func _on_Area2D_body_entered(body):
-	if body.name.begins_with("inimigo"):
-		print("u killed the bad guy!!!")
+	if body.is_in_group("inimigos"):
+		print("u killed deh enemymm")
 	
 	queue_free()
 	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	position += (Vector2.RIGHT*bulletspeed).rotated(rotation) * _delta
+
