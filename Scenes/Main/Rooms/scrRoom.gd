34e67b2352410b0eaa2f5a4eda02d00d17169d4b
TheMasterLucas <dev.lucas.angelo@gmail.com>
2021-07-31 17:06:35 -0300

    enemy despawn and level togle fix

--- Scenes/Main/Rooms/scrRoom.gd
+++ Scenes/Main/Rooms/scrRoom.gd
@@ -4,9 +4,15 @@ export(Rect2) var camera_bounds = Rect2(0, 0, 0, 0)
 
 var enemy_amount = 0
 var count_enemies = false
+var player_got_here = false
 
 func _ready():
-	$Entities.pause_mode = Node.PAUSE_MODE_STOP
+	if not $Entities.get_child_count() == 0:
+		for enemy in $Entities.get_children():
+			enemy.set_process(false)
+			enemy.set_physics_process(false)
+			print("desactivated " + enemy.name)
+	
 	visible = false
 func _process(delta):
 	if count_enemies:
@@ -15,14 +21,13 @@ func _process(delta):
 			get_parent().get_parent().get_node("Audio").action_amount = 0 
 			count_enemies = false
 	
-	for layer in $Sprites/ParallaxBackground.get_children():
-		layer.visible = visible
+	for node in $Sprites.get_children():
+		if node.name.begins_with("ParallaxBackground"):
+			for layer in node.get_children():
+				layer.visible = visible
 
 func _on_Bound_area_exited(area):
 	if area.is_in_group("PlayerLevelHitbox"):
-		
-		$Entities.pause_mode = $Entities.PAUSE_MODE_STOP
-		$Objects.pause_mode = $Objects.PAUSE_MODE_STOP
 		visible = false
 	
 	if area.is_in_group("Bullet"):
@@ -30,12 +35,18 @@ func _on_Bound_area_exited(area):
 func _on_Bound_area_entered(area):
 	if area.is_in_group("PlayerLevelHitbox"):
 		count_enemies = true
+		
 		GameManager.camera.limit_left = camera_bounds.position.x
 		GameManager.camera.limit_top = camera_bounds.position.y
 		GameManager.camera.limit_right  = camera_bounds.size.x
 		GameManager.camera.limit_bottom  = camera_bounds.size.y
 		
+		if not player_got_here:
+			if not $Entities.get_child_count() == 0:
+				for enemy in $Entities.get_children():
+					enemy.set_process(true)
+					enemy.set_physics_process(true)
+					print("activated " + enemy.name)
 		
-		$Entities.pause_mode = $Entities.PAUSE_MODE_INHERIT
-		$Objects.pause_mode = $Objects.PAUSE_MODE_INHERIT
+		player_got_here = true
 		visible = true
